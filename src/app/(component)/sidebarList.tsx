42fc308e9e21f26db4e2e1d978be11f0e9c3f670
embir25 <embir25@gmail.com>
2024-01-20 16:55:22 +0000

    Added key props div element

--- src/app/(component)/sidebarList.tsx
+++ src/app/(component)/sidebarList.tsx
@@ -24,7 +24,7 @@ const MainMenus = ({}) => {
   return (
     <div>
       {sidebarMenus.map((menu, key) => (
-        <div  >
+        <div  key={menu.id}>
           <div className="w-full">
             <li
              key={menu.id}
