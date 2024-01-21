139ad8911b371cc0145e112f4a0c1c4977078957
embir25 <embir25@gmail.com>
2024-01-21 10:12:09 +0000

    routie route required filled removed

--- src/Model/Routine/routine.ts
+++ src/Model/Routine/routine.ts
@@ -4,13 +4,13 @@ const routineSchema = new mongoose.Schema(
   {
     school: { type: mongoose.Schema.Types.ObjectId, ref: "School" },
     class: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Class", required: true },
+      { type: mongoose.Schema.Types.ObjectId, ref: "Class"},
     ],
     section: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Section", required: true },
+      { type: mongoose.Schema.Types.ObjectId, ref: "Section"},
     ],
     subject: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Subject", required: true },
+      { type: mongoose.Schema.Types.ObjectId, ref: "Subject" },
     ],
     day: { type: String },
     begintime: { type: Date },
