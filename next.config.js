9333f60fc417d99b36421aa37a9454a52c4ee11d
Guy Richie <47226300+Qwabs@users.noreply.github.com>
2024-02-14 15:12:15 +0000

    Update next.config.js
    Made changes to the Next config files

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
             source: "/api/:path*",
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: 'https://edmas.vercel.app', },
+              { key: "Access-Control-Allow-Origin", value: 'https://edmas-app-nifad.ondigitalocean.app', },
               { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
             ]
