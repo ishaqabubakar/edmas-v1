0dcc90ff41d138f739575e425f6f850e72a5ae15
Guy Richie <47226300+Qwabs@users.noreply.github.com>
2024-02-14 16:10:13 +0000

    Update AXIOS.ts

--- src/API/AXIOS.ts
+++ src/API/AXIOS.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API_BASE_URL =
   process.env.NODE_ENV ==="production"
-    ? "https://edmas.vercel.app/api"
+    ? "https://edmas-uwtvz.ondigitalocean.app/api"
     : "http://localhost:3000/api";
 
 export const axiosInstance = axios.create({
