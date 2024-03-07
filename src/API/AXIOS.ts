d6bd35b13359a1b7bbb46c8d3879ec24aa2a94f2
Guy Richie <47226300+Qwabs@users.noreply.github.com>
2024-03-07 13:38:47 +0000

    Update AXIOS.ts

--- src/API/AXIOS.ts
+++ src/API/AXIOS.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const API_BASE_URL =
   process.env.NODE_ENV ==="production"
-    ? "https://edmas-uwtvz.ondigitalocean.app/api"
-    : "http://localhost:3000/api";
+    ? "https://edmas-mus6a.ondigitalocean.app/api"
+    : "http://localhost:9090/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
