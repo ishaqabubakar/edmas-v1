7c616c577882783a27817003c0d14c9af9329350
embir25 <embir25@gmail.com>
2024-04-09 11:59:28 +0000

    Fix building idssue #Modules not found

--- src/app/(component)/tables/ownersTable.tsx
+++ src/app/(component)/tables/ownersTable.tsx
@@ -15,7 +15,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
+} from "../../../components/ui/table";
 import { UserContext } from "@/contextAPI/generalContext";
 import { Edit, Eye, MoreHorizontal, SortAsc, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
