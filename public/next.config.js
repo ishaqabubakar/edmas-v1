6ea6bdfec82de92e159a37a6674e3f30601dc3c3
Francis Sarpaning <infoajumapro@gmail.com>
2024-05-31 14:39:45 +0000

    ADD
    add

---
+++ public/next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const corsOrigins = process.env.NODE_ENV==="production"? 'https://dashboard.edmas.app':'http://localhost:3000/'
+const nextConfig = {
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
+      async headers() {
+        return [
+          {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value:corsOrigins},
+              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+          }
+        ]
+      }
+}
+
+module.exports = nextConfig
---
+++ public/tailwind.config.ts
@@ -0,0 +1,103 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+   
+      colors: {
+        brand: {
+          DEFAULT: "#048A81",
+          Dark: "#393833",
+          black: "#1D1D1D",
+          "gray-100": "#F7F7F7",
+          "gray-200": "#5D5D5D",
+          "gray-250": "#A1A1A1",
+          "gray-500": "#848484",
+          "gray-50": "#F4F4F5",
+          blue: "#2684ff",
+          icon: "#565656",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      fontFamily: {
+        poppins: ["var(--font-sans)"]
+      },
+      fontWeight: {
+        Thin: 100,
+        "Extra-light": 200,
+        Regular: 400,
+        Medium: 500,
+        "Semi-bold": 600,
+        Bold: 700,
+        "Extra-bold": 800,
+        Black: 900,
+      },
+      borderRadius: {
+        lg: "20px",
+        md: "10px",
+        sm: "5px",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
