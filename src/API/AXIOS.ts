3d6c2811a9bd228a07d1603519fe0d65b34ae7a6
Abubakar Ishaq <144918707+ishaqabubakar@users.noreply.github.com>
2024-06-12 16:47:11 +0000

    Update AXIOS.ts

--- src/API/AXIOS.ts
+++ src/API/AXIOS.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API_BASE_URL =
   process.env.NODE_ENV ==="production"
-    ? "https://dashboard.edmas.app/api"
+    ? "https://edmasv1.netlify.app/api"
     : "http://localhost:3000/api";
 
 export const axiosInstance = axios.create({
