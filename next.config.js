705d9ec270212ebfde9f5310efa0d2114ccf895b
ishaqabubakar <ishaq@gitcall.io>
2024-06-12 23:04:51 +0000

    restored

--- next.config.js
+++ next.config.js
@@ -3,18 +3,23 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const corsOrigins = process.env.NODE_ENV === "production" ? 'https://dashboard.edmas.app' : 'http://localhost:3000/'
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
@@ -25,17 +30,18 @@ const nextConfig = {
       }
     ]
   },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   webpack: (config, { isServer }) => {
-    // Add this to resolve aliases
     config.resolve.alias['@'] = path.resolve(__dirname, './src');
 
-    // A fix for 'fs' module not found error during the build. Remove if not necessary.
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
 
     return config;
   },
+  output: 'export', // This will output static files for GitHub Pages
 }
 
 module.exports = nextConfig
